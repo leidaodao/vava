8663054525e0cb4a4a7de93a111a0af61d87961e
leidaojun <1148166172@qq.com>
2019-06-09 20:13:47 +0800

    再次提交

--- src/cn/itcast/Test01.java
+++ src/cn/itcast/Test01.java
@@ -4,4 +4,8 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("你是傻逼");
     }
+    public void  test(){
+        int a=10;
+        System.out.println(a);
+    }
 }
